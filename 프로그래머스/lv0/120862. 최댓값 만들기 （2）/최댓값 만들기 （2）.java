3587128d4076a13a394618723aba5e62a6d4e56c
yshong1998 <106438992+yshong1998@users.noreply.github.com>
2023-01-30 11:31:43 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.34 ms, Memory: 67.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+class Solution {
+    public static int solution(int[] numbers) {
+        Arrays.sort(numbers);
+        return Math.max(numbers[0] * numbers[1], numbers[numbers.length - 2] * numbers[numbers.length - 1]);
+    }
+}

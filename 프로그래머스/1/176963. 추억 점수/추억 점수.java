ff03c0965af353ebca88528f843c7246fa7ac528
yshong1998 <106438992+yshong1998@users.noreply.github.com>
2024-06-03 16:51:19 +0900

    [level 1] Title: 추억 점수, Time: 0.03 ms, Memory: 69.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
+        Map<String, Integer> yearningMap = new HashMap<>();
+        for (int i = 0; i < name.length; i++) {
+            yearningMap.put(name[i], yearning[i]);
+        }
+        for (int i = 0; i < photo.length; i++) {
+            int sum = 0;
+            for (int j = 0; j < photo[i].length; j++) {
+                if (yearningMap.containsKey(photo[i][j])){
+                    sum += yearningMap.get(photo[i][j]);
+                }
+            }
+            answer[i] = sum;
+        }
+        return answer;
+    }
+}

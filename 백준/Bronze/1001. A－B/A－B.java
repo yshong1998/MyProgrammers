9fdd7eabf66ea012e7a1e22b7bfe0aa3f3001886
yshong1998 <106438992+yshong1998@users.noreply.github.com>
2025-04-14 01:40:38 +0900

    [Bronze V] Title: A-B, Time: 88 ms, Memory: 12824 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        String[] s = input.split(" ");
+        System.out.println(Integer.parseInt(s[0]) - Integer.parseInt(s[1]));
+    }
+}

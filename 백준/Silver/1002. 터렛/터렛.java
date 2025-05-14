d85acc9575beef4b22dd59b7b40bd7d79d7ef5e2
yshong1998 <106438992+yshong1998@users.noreply.github.com>
2025-05-14 20:33:14 +0900

    [Silver III] Title: 터렛, Time: 132 ms, Memory: 14912 KB -BaekjoonHub

--- 백준/Silver/1002. 터렛/터렛.java
+++ 백준/Silver/1002. 터렛/터렛.java
@@ -1,32 +1,25 @@
 import java.io.*;
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-
-        for (int i = 0; i < a; i++) {
-            int x1 = scanner.nextInt();
-            int y1 = scanner.nextInt();
-            int r1 = scanner.nextInt();
-            int x2 = scanner.nextInt();
-            int y2 = scanner.nextInt();
-            int r2 = scanner.nextInt();
-
-            double distance = Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
-
-            if (distance == 0 && r1 == r2) {
-                System.out.println(-1);
-            } else if (Math.abs(r1 - r2) == distance || r1 + r2 == distance) {
-                System.out.println(1);
-            } else if (Math.abs(r1 - r2) < distance && distance < (r1 + r2)) {
-                System.out.println(2);
-            } else {
-                System.out.println(0);
-            }
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            int answer = solution(arr[0], arr[1], arr[2], arr[3], arr[4], arr[5]);
+            bw.write(answer + "\n");
         }
-        scanner.close();
+        bw.close();
+    }
+
+    public static int solution(int x1, int y1, int r1, int x2, int y2, int r2){
+        double length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        if (length == 0 && r1 == r2) return -1; // 동심원 + 반지름이 같음
+        else if (length == 0) return 0; // 동심원 + 반지름이 다름
+        else if (length == Math.abs(r1 - r2) || length == r1 + r2) return 1; // 내접 || 외접하며 만남
+        else if (length > Math.abs(r1 - r2) && length < r1 + r2) return 2; // 두 교점에서 만남
+        else return 0; // 만나지 않음
     }
 }
